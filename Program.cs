85eaecc5dbf544f260825fdd682452aeb2007bde
MiguelViapiana <127536583+MiguelViapiana@users.noreply.github.com>
2024-03-21 11:27:10 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Produto> produtos = new List<Produto>();
 
 //Funcionalidades da aplicação - EndPoints
 app.MapGet("/produtos/listar", () => "Listagem de produtos");
 
-app.MapGet("/produtos/cadastrar", () => "Cadastro de produtos");
+//EXERCÍCIO - Cadastar produtos dentro da lista
+app.MapPost("/produtos/cadastrar", () => "Cadastro de produtos");
 
 app.Run();
+
+record Produto(string nome, string descricao);
